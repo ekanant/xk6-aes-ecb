b204d750f156451ef0ebec85546e9af360ed503d
Ekanant Phanuwatyingyong <osacn_ekanant.phanuwatyingyong@krungthai.com>
2025-10-01 17:17:59 +0700

    Create EncryptWithPad and DecryptWithUnPad

--- aes_ecb/module.go
+++ aes_ecb/module.go
@@ -25,6 +25,14 @@ func (AesEcb) Encrypt(plainText, key []byte) ([]byte, error) {
 	return cipherText, nil
 }
 
+func (c AesEcb) EncryptWithPad(plainText, key []byte, blockSize int, padStyle string) ([]byte, error) {
+	plainTextPad, err := c.Pad(plainText, blockSize, padStyle)
+	if err != nil {
+		panic(err)
+	}
+	return c.Encrypt(plainTextPad, key)
+}
+
 // AES-ECB decrypt
 // The key argument must be the AES key, either 16, 24, or 32 bytes to select AES-128, AES-192, or AES-256.
 func (AesEcb) Decrypt(cipherText, key []byte) ([]byte, error) {
@@ -41,4 +49,17 @@ func (AesEcb) Decrypt(cipherText, key []byte) ([]byte, error) {
 		block.Decrypt(plainText[start:start+block.BlockSize()], cipherText[start:start+block.BlockSize()])
 	}
 	return plainText, nil
+}
+
+func (c AesEcb) DecryptWithUnPad(cipherText, key []byte, blockSize int, padStyle string) ([]byte, error) {
+	enCryptedWithPad, err := c.Decrypt([]byte(cipherText), key)
+	if err != nil {
+		panic(err)
+	}
+
+	enCryptedUnPad, err := c.Unpad(enCryptedWithPad, blockSize, padStyle)
+	if err != nil {
+		panic(err)
+	}
+	return enCryptedUnPad, nil
 }
